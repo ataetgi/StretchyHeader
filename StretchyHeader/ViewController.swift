b9dbe06fbb71147a6168dfda40d7633df5ee7b57
ataetgi <ataetgi@icloud.com>
2021-04-23 23:46:06 +0300

    Initial Commit

---
+++ StretchyHeader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchyHeader
+//
+//  Created by Ata Etgi on 23.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
